4c1c598a02b7b94b6c281d50903696ae9dc84847
Vishank Dhiraj Rao Bagley B N <vishankdhiraj@gmail.com>
2025-10-11 23:31:57 +0530

    German Added

---
+++ src/component/world/German.jsx
@@ -0,0 +1,8 @@
+export default function German() {
+  return (
+    <div>
+      <h1>Willkommen in der Welt!</h1>
+      <p>Dies ist die deutsche Version der Weltkomponente.</p>
+    </div>
+  );
+}
